1b736b56193870dfa5e6b536391b2d6e9416b34c
jan <you@example.com>
2019-03-26 22:43:01 +0100

    test

--- js/app.js
+++ js/app.js
@@ -852,9 +852,11 @@ tbApp.controller('taskboardController', function ($scope, $filter, $http) {
     }
 
     var getVersion = function () {
+        alert($scope.version_number)
         $http.get(VERSION_URL)
             .then(function(response) {
                 $scope.version_number = response.data;
+                alert($scope.version_number)
                 $scope.version_number = $scope.version_number.replace(/\n|\r/g, "");
                 alert(response.data)
                 checkVersion();
