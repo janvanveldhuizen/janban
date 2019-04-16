4fc8bf02292017c2e0cd0c2677a802ea04c749b4
jan <you@example.com>
2019-04-17 00:09:05 +0200

    temp

--- setup.cmd
+++
@@ -1,3 +0,0 @@
-@echo off
-if not exist %USERPROFILE%\janban (mkdir %USERPROFILE%\janban)
-robocopy /mir . %USERPROFILE%\janban 
---
+++ uninstall.cmd
@@ -0,0 +1,9 @@
+if exist %USERPROFILE%\janban (rmdir %USERPROFILE%\janban /s /q)
+set offver=0
+for /l %%x in (12, 1, 19) do (
+      reg export HKCU\Software\Microsoft\Office\%%x.0\Outlook\Today %%x /y
+	  if exist %%x set offver=%%x
+)
+del %offver%
+reg add HKCU\Software\Microsoft\Office\%offver%.0\Outlook\Today /v Stamp /t REG_DWORD /d 0 /f
+reg add HKCU\Software\Microsoft\Office\%offver%.0\Outlook\Today /v UserDefinedUrl /t REG_SZ /d "" /f
