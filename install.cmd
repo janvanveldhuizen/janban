9eb7464a9baf2a18d4ec60635a820b90c7c1d99c
Jan van Veldhuizen <jan@van-veldhuizen.nl>
2019-05-01 07:57:19 +0200

    fixed install script

--- install.cmd
+++ install.cmd
@@ -11,6 +11,14 @@ for /l %%x in (12, 1, 19) do (
       reg export HKCU\Software\Microsoft\Office\%%x.0\Outlook\Today %%x /y
 	  if exist %%x set offver=%%x
 )
-del %offver%
-reg add HKCU\Software\Microsoft\Office\%offver%.0\Outlook\Today /v Stamp /t REG_DWORD /d 1 /f
-reg add HKCU\Software\Microsoft\Office\%offver%.0\Outlook\Today /v UserDefinedUrl /t REG_SZ /d %USERPROFILE%\janban\kanban.html /f
+if %offver%==0 (
+      echo  The install script could not detect your Office version. Please install manually.
+      pause
+      goto :EOF
+)
+if not %offver%==0 (
+      del %offver%
+      reg add HKCU\Software\Microsoft\Office\%offver%.0\Outlook\Today /v Stamp /t REG_DWORD /d 1 /f
+      reg add HKCU\Software\Microsoft\Office\%offver%.0\Outlook\Today /v UserDefinedUrl /t REG_SZ /d %USERPROFILE%\janban\kanban.html /f
+      goto :EOF
+)
--- uninstall.cmd
+++ uninstall.cmd
@@ -5,6 +5,8 @@ for /l %%x in (12, 1, 19) do (
       reg export HKCU\Software\Microsoft\Office\%%x.0\Outlook\Today %%x /y
 	  if exist %%x set offver=%%x
 )
-del %offver%
-reg add HKCU\Software\Microsoft\Office\%offver%.0\Outlook\Today /v Stamp /t REG_DWORD /d 0 /f
-reg add HKCU\Software\Microsoft\Office\%offver%.0\Outlook\Today /v UserDefinedUrl /t REG_SZ /d "res://C:\Program Files\Microsoft Office\root\Office%offver%\1033\OUTLWVW.DLL/outlook.htm" /f
+if not %offver%==0 (
+      del %offver%
+      reg add HKCU\Software\Microsoft\Office\%offver%.0\Outlook\Today /v Stamp /t REG_DWORD /d 0 /f
+      reg add HKCU\Software\Microsoft\Office\%offver%.0\Outlook\Today /v UserDefinedUrl /t REG_SZ /d "res://C:\Program Files\Microsoft Office\root\Office%offver%\1033\OUTLWVW.DLL/outlook.htm" /f
+)
